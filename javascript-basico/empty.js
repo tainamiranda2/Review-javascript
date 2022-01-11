ba61655fc8586bb0a9b51e0eb46f65f47cc42697
Tainá Miranda <Tainamirandaferreira@gmail.com>
2022-01-11 11:03:23 +0000

    Operador ternario

---
+++ javascript-basico/empty.js
@@ -0,0 +1,10 @@
+//return undefined e null
+console.log(null)
+console.log(undefined)
+
+console.log(5 * null)
+//retorna 0
+console.log("10" - 4)
+//retorna 6
+console.log("5" * 3)
+//retorna 15
---
+++ javascript-basico/operadorTernario.js
@@ -0,0 +1,6 @@
+console.log(5 > 2 ? 'Sim' : 'Não')
+//ter que ser Sim
+console.log(false ? 5:4)
+//ter que  ser 4
+console.log("Tainá" == "Tainá" ? "Olá, Tainá" : "Não é Tainá")
+//comparação com o operador ternario
