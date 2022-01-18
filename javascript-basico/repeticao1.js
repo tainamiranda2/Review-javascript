7c5e8770b3536160d372d16195c2d3a36661f506
Tainá Miranda <Tainamirandaferreira@gmail.com>
2022-01-18 11:11:14 +0000

    Estrutura de repetição

---
+++ javascript-basico/repeticao1.js
@@ -0,0 +1,14 @@
+let x =10;
+while(x > 0){
+  //exibi x
+  console.log(x)
+//va decrementando
+  x= x -1;
+}
+
+//variação do while
+let y=0;
+do {
+  console.log(y)
+y=y +1
+}while (y < 5)
---
+++ javascript-basico/repeticao2.js
@@ -0,0 +1,4 @@
+//variavel, condição e incremento
+for (let i =0; i < 50; i =i +3){
+  console.log(`A soma de i com 2 é: ${i+2}`)
+}
