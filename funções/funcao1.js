3ee44e7dd4efa771eabe2e01473095a706c4c6a8
Tainá Miranda <Tainamirandaferreira@gmail.com>
2022-01-31 11:19:32 +0000

    Iniciado estudos das funções

---
+++ funções/funcao1.js
@@ -0,0 +1,28 @@
+//codigo reaproveitados, poupar repetiçaõ, são subprogrmas.
+
+//função argumento
+function exibir(){
+console.log("ola")
+}
+//invocando
+exibir()
+let num=1, num2=9;
+console.log(num, num2)
+
+//com parametro
+function soma(num){
+  console.log(num)
+}
+//quantas vezes eu quiser
+
+soma(2)
+soma(3)
+soma(4)
+
+//deixando estrutura de programação
+
+function mul(x,y,z){
+return x* y *z;
+}
+
+console.log(mul(4, 1,6))
