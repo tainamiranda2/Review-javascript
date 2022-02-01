bda7588a34d81a4100f4cfa6319035c0423a9dc6
Tainá Miranda <Tainamirandaferreira@gmail.com>
2022-02-01 11:16:57 +0000

    Estudo das arrow functions.

---
+++ funções/arrowFunction.js
@@ -0,0 +1,18 @@
+//uma forma de escreve função e usava com framworks
+
+let consoleTeste = () =>{
+  console.log("Ola")
+}
+consoleTeste()
+
+let soma =(a, b)=>{
+  return a + b;
+}
+
+console.log(soma(2, 8))
+
+//se tem apenas um parametro e se é pequena
+
+const multiplicar = x => x *2;
+
+console.log(multiplicar(4))
---
+++ funções/funcao2.js
@@ -0,0 +1,12 @@
+//escorpo 
+let n=20 //variavel gobal
+
+ function numero (){
+  let n = 25;
+  console.log(n)
+}
+//variavel globl
+console.log(n)
+//varivael interna
+numero()
+
