35aaad61709fd9da249146e191f5df0209f5ec74
Tainá Miranda <Tainamirandaferreira@gmail.com>
2022-02-21 11:24:53 +0000

    Mais sobre métodos com array.

---
+++ array-e-objeto/loopArray.js
@@ -0,0 +1,7 @@
+//muitos valores e precisamos saber cada um deles
+
+let nomes=['matheus', 'guga', 'Lucas'];
+
+for(let i=0; i < nomes.length; i++){
+  console.log(nomes[i])
+}
---
+++ array-e-objeto/metodosArray.js
@@ -0,0 +1,18 @@
+//pop, push, shift
+let nomes=["Matheus",  "Lucas", "Pedro"];
+//remove
+let elementoRemovido=nomes.pop()
+
+console.log(elementoRemovido);
+console.log("Nome removido:" +nomes)
+//adiciona 
+nomes.push("leo")
+console.log("nome colocado:" +nomes)
+
+//remover o primeiro
+
+let removerPrimeiro=nomes.shift()
+console.log("Nome no inicio removido:" +nomes)
+
+
+
