b0dd35aa4270890c5b36c84637c466b1ed23b70a
Tainá Miranda <Tainamirandaferreira@gmail.com>
2022-03-09 12:52:49 +0000

    Mais exercícios sobre array.

---
+++ exercicios-array/ex05.js
@@ -0,0 +1,7 @@
+array=["bruna", "Tainá", "Marcos", "Luan", "Pedro", "Leo"]
+
+if(array.includes("Ze")){
+  console.log("ok")
+}else{
+  console.log("Error")
+}
---
+++ exercicios-array/ex06.js
@@ -0,0 +1,16 @@
+let array1=[1,2,3,5,6,7];
+
+let array2=[2,4,6,8];
+
+function verificar(array) {
+  
+if(array.length >= 5){
+  console.log("Muitos elementos")
+}else{
+    console.log("Poucs elementos")
+
+}
+}
+
+verificar(array1);
+verificar(array2)
