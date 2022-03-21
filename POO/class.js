3d74b5299b1165e2cd747a4d04dcef2459b18e0a
Tainá Miranda <Tainamirandaferreira@gmail.com>
2022-03-21 11:51:13 +0000

    Entendendo classes

--- POO/class.js
+++ POO/class.js
@@ -1,1 +1,26 @@
 //Prototype também podem sem chamadoos de clases.
+//pode criar varios objetos  em cima do prototype
+
+let cachorro = {
+  patas:4,
+  raca: 'Viralata',
+  latir: function() {
+    console.log("Au au");
+  }
+}
+
+let labrador = Object.create(cachorro);
+
+//primeiro objeto cirado
+labrador.latir();
+labrador.raca="Labrador";
+
+console.log(labrador.raca);
+console.log(cachorro.raca);
+
+let pastor = Object.create(cachorro);
+
+pastor.latir();
+pastor.raca="Pastor Alemão";
+//segundo objeto cirado
+console.log(pastor.raca);
---
+++ POO/construtor.js
@@ -0,0 +1,2 @@
+//melhor maneira de criar um objeto
+//instaciar um objeto é o mesmo que crir uma class
