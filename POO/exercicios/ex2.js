d6462c36fde2aa0d3f87cfe52bc80eb1e57af5b6
Tainá Miranda <Tainamirandaferreira@gmail.com>
2022-04-06 12:07:41 +0000

    Erro

--- POO/exercicios/ex2.js
+++ POO/exercicios/ex2.js
@@ -1,41 +1,48 @@
 //e-commerce
 class E_commerce{
-  constructor(carrinho, valorTotal,itens){
-    this.carrinho=carrinho;
+  constructor(qtd,valorTotal,itens){
+this.qtd=qtd;
     this.valorTotal=valorTotal;
     this.itens=itens;
   }
   //metodo adicionar itens/produtos
-  Compras(item){
+  additem(item){
     //this.carrinho+=produto;
     for(let itemCarrinho in this.itens){
-      if(this.itens[itemCarrinho].id==itens.id){
-        this.itens[itemCarrinho].carrinho+=itens.carrinho;
+      if(this.itens[itemCarrinho].id==item.id){
+        this.itens[itemCarrinho].qtd+=item.qtd;
       }
-      this.carrinho+=item.carrinho;
-this.valorTotal+=itens.preco * item.carrinho;
+    }
+      this.qtd+=item.qtd;
+this.valorTotal+=item.preco * item.qtd;
     }
   }
   //metodo para remover itens
    /*Compras(produto){
     this.carrinho-=produto;
   }*/
-}
+
 
 let e_commerce= new E_commerce([
   {
     id: 01,
-    nome: "Açucar",
-    carrinho:2,
-    preco:3.60
-  }
-],
-                               3, 200);
+    nome: "Camisa",
+    qtd:1,
+    preco:40
+  },
+  
+],1, 120);
 
-/*e_commerce.Compras('Feijão');
-e_commerce.Compras('Salame');
-e_commerce.Compras('Pao');
-e_commerce.Compras('Queijo');
-*/
 console.log("Vendo o que tem no carrinho: ",e_commerce);
+//testando
+e_commerce.additem({
+  id:01,
+  nome: "Camisa",
+  qtd:3,
+  preco:20
+  
+})
 
+
+console.log("Vendo novamente",e_commerce)
+ 
