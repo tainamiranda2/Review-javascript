d079247bd4e0d6c015e77448560730f815a88d95
Tainá Miranda <Tainamirandaferreira@gmail.com>
2022-04-07 11:35:28 +0000

    Novo erro

--- POO/exercicios/ex2.js
+++ POO/exercicios/ex2.js
@@ -1,62 +1,25 @@
-//e-commerce
-class E_commerce{
-  constructor(qtd,valorTotal,itens){
-    
-this.qtd=qtd;
-this.valorTotal=valorTotal;
-this.itens=itens;
+class Carrinho {
+  constructor(itens, qtd, valort){
+    this.itens=itens;
+    this.qtd;
+    this.valort=valort;
   }
-  //metodo adicionar itens/produtos
-  additem(item){
-    let contador=0;
-    //this.carrinho+=produto;
-  for(let itemCarrinho in this.itens){
-    if(this.itens[itemCarrinho].id==item.id){
-      this.itens[itemCarrinho].qtd+=item.qtd;
-        contador=1;
-      }
-    }
-    if(contador ==0){
-      this.itens.push(item);
-    }
-this.qtd+=item.qtd;
-this.valorTotal+=item.preco * item.qtd;
-    }
-  }
-  
-//
-let e_commerce= new E_commerce([
+}
+
+//itens no carrinho
+let carrinho = new Carrinho([
   {
-    id: 01,
-    nome: "Camisa",
+    id:01,
+    nome:'Camisa',
     qtd:1,
     preco:20
   },
   {
-    id: 02,
-    nome: "Calça",
+    id:02,
+    nome:'Calça',
     qtd:1,
     preco:50
-  },
-  
-],3, 120);
+  }
+],3,120)
 
-//console.log(e_commerce);
-//testando
-e_commerce.additem({
-  id:01,
-  nome: "Camisa",
-  qtd:3,
-  preco:20
-  
-})
-e_commerce.additem({
-  id:02,
-  nome: "Calça",
-  qtd:3,
-  preco:40
-  
-})
-
-console.log(e_commerce)
- 
+console.log(carrinho)
