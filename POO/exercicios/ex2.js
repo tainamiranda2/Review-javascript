61a52a7b024b39718144063483b157706215b6a3
Tainá Miranda <Tainamirandaferreira@gmail.com>
2022-04-11 11:36:03 +0000

    Erro no  exercicio 2

--- POO/exercicios/ex2.js
+++ POO/exercicios/ex2.js
@@ -1,12 +1,44 @@
 class Carrinho {
+  //constructor
   constructor(itens, qtd, valort){
     this.itens=itens;
     this.qtd;
     this.valort=valort;
   }
+  
+  addItem(item){
+    //novo produto
+    let contador=0;
+    for(let itemCarrinho in this.itens){
+      if(this.itens[itemCarrinho].id==item.id){
+        this.itens[itemCarrinho].qtd+=item.qtd;
+        contador=1;
+      }
+    }
+    if(contador==0){
+      this.itens.push(item);
+    }
+    this.qtd+=item.qtd;
+    this.valort+=item.preco*item.qtd;
+  }
+  //remover
+  removerItem(item){
+   for(let itemCarrinho in this.itens){
+      if(this.itens[itemCarrinho].id==item.id){
+        let obj = this.item[itemCarrinho];
+        
+        let index = this.itens.findIndex(function(obj) {return obj.id==item.id})
+        
+    this.itens.splice(index, 1);
+        
+this.qtd-=this.item[itemCarrinho].qtd;
+this.valort-=this.itens[itemCarrinho].preco*this.itens[itemCarrinho].qtd;
+      }
+   }
 }
-
+}
 //itens no carrinho
+//instanciado
 let carrinho = new Carrinho([
   {
     id:01,
@@ -17,9 +49,33 @@ let carrinho = new Carrinho([
   {
     id:02,
     nome:'Calça',
-    qtd:1,
+    qtd:2,
     preco:50
   }
 ],3,120)
 
+//console.log(carrinho)
+
+carrinho.addItem({
+  id:01,
+  nome:'Camisa',
+  qtd:2, 
+  preco:20
+});
+
+//console.log(carrinho)
+
+carrinho.addItem({
+  id:03,
+  nome:'Boné',
+  qtd:1,
+  preco:15
+})
+console.log(carrinho)
+
+carrinho.removeItem({
+  id: 01, 
+  nome: 'Camisa',
+  qtd, 2, 
+  preco:20})
 console.log(carrinho)
