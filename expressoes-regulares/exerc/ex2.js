d57d149303bf7ee2986df8b25a3ce4860d1c504e
Tainá Miranda <Tainamirandaferreira@gmail.com>
2022-04-25 11:26:09 +0000

    fim da sessão regex e inicio de programação assíncrona.

---
+++ expressoes-regulares/exerc/ex2.js
@@ -0,0 +1,3 @@
+const validadarID= /\d+ID\b/
+
+console.log(validadarID.test("294006ID"));
---
+++ expressoes-regulares/exerc/ex3.js
@@ -0,0 +1,3 @@
+const validarIp=/\d{1-3}\.\/d{1-3}\.\/d{1-3}\.\/d{1-3}\.\/d{1-3}\./;
+
+console.log(validarIp.test('192.168.0.62'))
