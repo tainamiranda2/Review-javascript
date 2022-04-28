b31a88da5531658110962c8fbddb6b743fcbd87a
Tainá Miranda <Tainamirandaferreira@gmail.com>
2022-04-28 17:14:38 +0000

    Entendo o dom.

---
+++ navegador-js/script..js
@@ -0,0 +1,10 @@
+console.log(document.body);
+
+//mover os elementos atraes do dom
+
+console.log(document.boby);
+console.log(document.boby.childNodes)
+console.log(document.boby.childNodes[2]..childNodes)
+
+//encontrando elementos
+
