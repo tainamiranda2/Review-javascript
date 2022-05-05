de6c255ae7b43b3f329777e4f69f93dfc7d187ed
Tainá Miranda <Tainamirandaferreira@gmail.com>
2022-05-05 17:45:13 +0000

    continuação da sessão de eventos

---
+++ eventos/propagation.js
@@ -0,0 +1,20 @@
+let p = document.querySelector('p')
+
+let btn1=document.querySelector("#btn1");
+
+let btn2=document.querySelector("#btn2");
+
+function msg(){
+  console.log("Clicou no button");
+  e.stopPropagation();
+}
+//removenod evento
+
+btn1.addEventListener("click",msg)
+btn2.addEventListener("click",function(event) {
+  console.log(event);
+})
+
+p.addEventListener('click', function(){
+  console.log("Clicou no paragrafo");
+})
--- eventos/remove.js
+++ eventos/remove.js
@@ -1,3 +1,4 @@
+
 let btn1=document.querySelector("#btn1");
 
 let btn2=document.querySelector("#btn2");
@@ -5,9 +6,9 @@ let btn2=document.querySelector("#btn2");
 function msg(){
   console.log("Clicou em min")
 }
+//removenod evento
 
 btn1.addEventListener("click",msg)
-
 btn2.addEventListener("click", function(){
   btn1.removeEventListener("click", msg)
 })
